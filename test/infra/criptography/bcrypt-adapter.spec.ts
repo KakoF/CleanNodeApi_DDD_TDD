d3ac3fc704eac2b603fab6df9fe5a056adb4eca3
Kako Ferrare <kakoferrare87@gmail.com>
2023-01-18 15:17:16 -0300

    refactor: create function to create sut

--- test/infra/criptography/bcrypt-adapter.spec.ts
+++ test/infra/criptography/bcrypt-adapter.spec.ts
@@ -6,16 +6,21 @@ jest.mock('bcrypt', () => ({
     return await new Promise(resolve => { resolve('hash') })
   }
 }))
+
+const makeSut = (): BcryptAdapter => {
+  return new BcryptAdapter()
+}
+
 describe('Bcrypt Adapter', () => {
   test('Should call bcrypt with correct value', async () => {
-    const sut = new BcryptAdapter()
+    const sut = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', 12)
   })
 
   test('Should return hash on success', async () => {
-    const sut = new BcryptAdapter()
+    const sut = makeSut()
     const hashed = await sut.encrypt('any_value')
     expect(hashed).toBe('hash')
   })
