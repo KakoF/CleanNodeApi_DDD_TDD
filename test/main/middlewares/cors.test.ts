ec0cef6a6c07133184ba0f19ae283c7cce400597
Kako Ferrare <kakoferrare87@gmail.com>
2023-01-19 15:42:46 -0300

    refactor: test

--- test/main/middlewares/cors.test.ts
+++ test/main/middlewares/cors.test.ts
@@ -1,6 +1,5 @@
 import request from 'supertest'
 import app from '../../../src/main/config/app'
-
 describe('Cors Middleware', () => {
   test('should enable cors', async () => {
     app.get('/test_cors', (req, res) => {
@@ -9,5 +8,7 @@ describe('Cors Middleware', () => {
     await request(app)
       .get('/test_cors')
       .expect('access-control-allow-origin', '*')
+      .expect('access-control-allow-methods', '*')
+      .expect('access-control-allow-headers', '*')
   })
 })
