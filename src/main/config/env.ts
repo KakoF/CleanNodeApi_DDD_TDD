bdd667d3b28ccaf52b223b76aece0151031a0297
Kako Ferrare <kakoferrare87@gmail.com>
2023-01-20 15:18:08 -0300

    refactor: refactor

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,4 @@
 export default {
   mongUrl: process.env.MONGO_URL ?? 'mongodb://root:example@localhost:27017/clean_node_api?authSource=admin',
-  // mongUrl: process.env.MONGO_URL ?? 'mongo://root:example@mongo:27017/node_api_clean?authSource=admin',
-  // mongUrl: process.env.MONGO_URL ?? 'mongo://root:example@mongo:27017/clean_node_api',
   port: process.env.PORT ?? 5050
 }
